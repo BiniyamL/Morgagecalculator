6e2a23a257a92ef4d451b2d8ce126c54dc8831ff
Biniyam Legesse <biniyamlegesse1433@gmail.com>
2024-10-06 18:45:45 +0300

    morgage calculator -commiting all failes to the repository

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+import java.text.NumberFormat;
+import java.util.Scanner;
+
+public class Main {
+    private static final int MONTHS_IN_A_YEAR= 12;
+    public static void main(String[] args) {
+        Scanner scanner= new Scanner(System.in);
+
+        System.out.println("Enter The Principal Amount:");
+        double principal= scanner.nextDouble();
+        System.out.println("Enter the annual interest rate:");
+        float annualInterestRate= scanner.nextFloat();
+        System.out.println("Enter the term in year:");
+        byte termInYear= scanner.nextByte();
+        float monthlyInterestRate= annualInterestRate/MONTHS_IN_A_YEAR;
+        int numberOfPayment= termInYear*MONTHS_IN_A_YEAR;
+        double monthlyPayment= principal*
+                (monthlyInterestRate * (Math.pow(1+ monthlyInterestRate,numberOfPayment ))) /
+                ((Math.pow( 1 + monthlyInterestRate,numberOfPayment))-1);
+        System.out.println("Monthly payment is:" + NumberFormat.getCurrencyInstance().format(monthlyPayment));
+        System.out.println("total payment:" + NumberFormat.getCurrencyInstance().format((monthlyPayment*numberOfPayment)));
+    }
+}
